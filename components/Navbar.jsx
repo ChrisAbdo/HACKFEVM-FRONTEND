3689b881d8283e654d17a666a15bc2bdc649bb12
chris abdo <chris.j.abdo@gmail.com>
2022-11-19 12:16:27 -0500

    ADDED: simple Web3Handler in _app.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
-
-const Navbar = () => {
+import Web3 from "web3";
+const Navbar = ({ Web3Handler, account }) => {
   // make the navbar fade into white when scrolled down at least 1 pixel
   const [scroll, setScroll] = useState(false);
 
@@ -131,14 +131,16 @@ const Navbar = () => {
             Claim
           </a>
         </div>
-        <a
-          href="#_"
+        <div
+          onClick={() => {
+            Web3Handler();
+          }}
           className="relative inline-block px-4 py-2 font-medium group "
         >
           <span className="absolute rounded-lg inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-[#bff22d] border-[2px] border-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
           <span className="absolute rounded-lg inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-[#bff22d]"></span>
           <span className="relative text-black">Connect Wallet</span>
-        </a>
+        </div>
       </div>
     </div>
   );
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,61 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import { useState, useEffect } from "react";
+import Web3 from "web3";
 
 function MyApp({ Component, pageProps }) {
+  const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  useEffect(() => {
+    loadWeb3();
+  }, []);
+
+  const loadWeb3 = async () => {
+    try {
+      const web3 = new Web3(window.ethereum);
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const Web3Handler = async () => {
+    try {
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: "0x7ab7",
+            chainName: "Wallaby Testnet",
+            nativeCurrency: {
+              name: "FIL",
+              symbol: "FIL",
+              decimals: 18,
+            },
+            rpcUrls: ["https://wallaby.node.glif.io/rpc/v0"],
+            blockExplorerUrls: ["https://filscout.com/"],
+          },
+        ],
+      });
+      const account = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      const web3 = new Web3(window.ethereum);
+      setAccount(account[0]);
+      setWeb3(web3);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <div className="bg-gradient-to-r from-[#fdeab1] to-white min-h-screen">
-        <Navbar />
-        <Component {...pageProps} />
+        <Navbar Web3Handler={Web3Handler} account={account} />
+        <Component {...pageProps} Web3Handler={Web3Handler} account={account} />
       </div>
     </>
   );
